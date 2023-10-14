60d0c79160a1f2ad533fdf45702169c04cdad29c
Malone <bmalo4@illinois.edu>
2023-10-14 15:24:49 -0500

    progress on paging init

--- student-distrib/paging.c
+++ student-distrib/paging.c
@@ -1,2 +1,78 @@
 #include "paging.h"
 
+/* page_directory is our entire page directory object, this function initializes it */
+/* 
+    INPUTS : None
+    OUTPUTS : None
+    RETURNS : 0 on success, -1 on failure
+    SIDE EFFECTS : initializes the page directory object
+*/
+int init_paging(){
+    int i, j;
+    /* 0th entry points to the 0-4MB section that encapsulates the video memory */
+    PTE page_table_zero[DIR_TABLE_SIZE] = default_page_table; //makes a page table for this PDE
+    page_directory.dir[0].present = 1;
+    page_directory.dir[0].address = 0; //should be zero since its the first page table
+    page_directory.dir[0].avail = 0; 
+    page_directory.dir[0].global = 1; //we dont flush the kernel page so it's global
+    page_directory.dir[0].p_size = 1; //4kb pages
+    page_directory.dir[0].reserved = 0;
+    page_directory.dir[0].accessed = 0; //starts as zero since this page table is not yet accessed
+    page_directory.dir[0].cache_dis = 0;
+    page_directory.dir[0].write_through = 0;
+    page_directory.dir[0].user_super = 0; //this is the video mem so level 0 privelage
+    page_directory.dir[0].read_write = 1; //assume read/write enabled
+    /* Now I need to fill the entire page table for this first PDE */
+    for (i = 0; i < DIR_TABLE_SIZE; i++){
+        if(i > VIDEO_MEM_BEGIN && i < VIDEO_MEM_END){
+            page_table_zero[i].present = 1;
+            page_table_zero[i].address = i * FOUR_KB; //TODO: THIS IS BASED ON PAGE ADDRESSING 
+            page_table_zero[i].avail = 0;
+            page_table_zero[i].global = 1; // we dont want to flush
+            page_table_zero[i].ptai = 0;
+            page_table_zero[i].dirty = 0;
+            page_table_zero[i].accessed = 0;
+            page_table_zero[i].cache_dis = 0;
+            page_table_zero[i].write_through = 0;
+            page_table_zero[i].user_super = 0;
+            page_table_zero[i].read_write = 1;
+        }
+        page_table_zero[i].present = 0;
+        page_table_zero[i].address = i * FOUR_KB; //TODO: THIS IS BASED ON PAGE ADDRESSING 
+        page_table_zero[i].avail = 0;
+        page_table_zero[i].global = 0;
+        page_table_zero[i].ptai = 0;
+        page_table_zero[i].dirty = 0;
+        page_table_zero[i].accessed = 0;
+        page_table_zero[i].cache_dis = 0;
+        page_table_zero[i].write_through = 0;
+        page_table_zero[i].user_super = 0;
+        page_table_zero[i].read_write = 0;
+    }
+    // TODO ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    /* 1st entry is the 4MB single kernel page 
+        PDE for a 4MB page is structured differently
+        [31:22]Page Base Address, [21:13]Reserved, [12]ptai, [11:9] avail,
+        [8]global, [7]p_size, [6]dirty, [5]accessed, [4]accessed,
+        [3]cache_dis, [2]user_super, [1]read_write, [0]present 
+    */
+    page_directory.dir[1].present = 1;
+    /* the address space is split for this PDE only since its a 4MB page */
+    /* [31:22]Page Base Address, [21:13]Reserved, [12]ptai is the new breakdown */
+    page_directory.dir[1].address = KERNEL_ADDR; //TODO: what should the page base address be
+    page_directory.dir[1].avail = 0; 
+    page_directory.dir[1].global = 1; //we dont flush the kernel page so it's global
+    page_directory.dir[1].p_size = 1; //4MB page
+    page_directory.dir[1].reserved = 0;
+    page_directory.dir[1].accessed = 0; //starts as zero since this page table is not yet accessed
+    page_directory.dir[1].cache_dis = 0; //TODO do we need this?
+    page_directory.dir[1].write_through = 0; //TODO do we need this?
+    page_directory.dir[1].user_super = 0; //this is the kernel page so level 0 privelage
+    page_directory.dir[1].read_write = 1; //assume read/write enabled
+
+    /* initial pages and page tables from 8MB - 4GB */
+    for (j = 0; j < DIR_TABLE_SIZE; j++){
+        /* j is indexing in the page directory */
+        
+    }
+}
--- student-distrib/paging.h
+++ student-distrib/paging.h
@@ -3,14 +3,12 @@
 #define DIR_TABLE_SIZE  1024
 #define FOUR_KB         4096
 #define ENTRY_SIZE      4
+#define VIDEO_MEM_BEGIN 
+#define VIDEO_MEM_END   
+#define KERNEL_ADDR     0x400000
 
 /* steal the similar struct formats for page dir and page tables 
 TODO: how do I used __attribute__((packed))/ do I even need to? */
-
-/* Page Directory */
-typedef struct __attribute__ ((packed)) page_dir{
-    PDE dir[DIR_TABLE_SIZE];
-} page_dir;
 
 /* Page Directory Entry 
     - 1024 of these makes up THE page directory
@@ -18,9 +16,7 @@ typedef struct __attribute__ ((packed)) page_dir{
 typedef union PDE{
     uint32_t val[1];
     struct {
-        /* communicates if the particular table exists from this PDE*/
-        uint32_t present : 1;
-        /* SEE PAGE 50-51 of IA-32 for these variables of PDE */
+        /* SEE PAGE 90-91 of IA-32 for these variables of PDE */
         uint32_t address : 20; // [12:31]
         uint32_t avail : 3; 
         uint32_t global : 1; 
@@ -33,6 +29,8 @@ typedef union PDE{
         uint32_t write_through : 1;
         uint32_t user_super : 1;
         uint32_t read_write : 1;
+        /* communicates if the particular table exists from this PDE*/
+        uint32_t present : 1;
     } __attribute__ ((packed));
 } PDE;
 
@@ -42,8 +40,6 @@ typedef union PDE{
 typedef union PTE{
     uint32_t val[1];
     struct {
-        /* communicates if the particular page exists from this PTE*/
-        uint32_t present : 1;
         /* SEE PAGE 50-51 of IA-32 for these variables of PDE */
         uint32_t address : 20; // [12:31]
         uint32_t avail : 3; 
@@ -56,13 +52,20 @@ typedef union PTE{
         uint32_t write_through : 1;
         uint32_t user_super : 1;
         uint32_t read_write : 1;
+        /* communicates if the particular page exists from this PTE*/
+        uint32_t present : 1;
     } __attribute__ ((packed));
 } PTE;
 
+/* Page Directory */
+typedef struct __attribute__ ((packed)) page_dir{
+    PTE* dir[DIR_TABLE_SIZE];
+} page_dir;
+
 /* our aligned page directory object and default aligned PDE/PTE objects */
-page_dir page_directory __attribute__((aligned(FOUR_KB)));
-PDE default_pde __attribute__((aligned(ENTRY_SIZE)));
-PTE default_pte __attribute__((aligned(ENTRY_SIZE)));
+page_dir page_directory __attribute__((aligned(FOUR_KB))); // TODO: do we put this inside the struct
+// PDE default_page_directory[DIR_TABLE_SIZE] __attribute__((aligned(FOUR_KB))); 
+PTE default_page_table[DIR_TABLE_SIZE] __attribute__((aligned(FOUR_KB)));
 
 /* function that instantiates out pages:
     we need: single 4MB page for the kernel (from 4MB - 8MB)
